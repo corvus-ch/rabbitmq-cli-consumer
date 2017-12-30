4bde7a5e370e1b4dfe39b84e5ebabeede9a13d7c
Christian Haeusler <christian.haeusler@bexio.com>
2017-12-30 22:38:18 +0100

    Merge testing command into test file

--- main_test.go
+++ main_test.go
@@ -4,17 +4,23 @@ package main_test
 
 import (
 	"bytes"
+	"compress/zlib"
+	"encoding/base64"
+	"flag"
 	"fmt"
+	"io"
 	"io/ioutil"
 	"os"
 	"os/exec"
+	"strings"
 	"testing"
 	"time"
 
 	"github.com/sebdah/goldie"
 	"github.com/streadway/amqp"
-	"strings"
 )
+
+var command = os.Args[0] + " -test.run=TestHelperProcess -- "
 
 var tests = []struct {
 	name string
@@ -29,35 +35,35 @@ var tests = []struct {
 }{
 	{
 		"default",
-		[]string{"-V", "-no-datetime", "-e", "go run test/command.go", "-c", "test/default.conf"},
+		[]string{"-V", "-no-datetime", "-e", command, "-c", "fixtures/default.conf"},
 		"test",
 		amqp.Publishing{ContentType: "text/plain", Body: []byte("default")},
 		[]string{},
 	},
 	{
 		"compressed",
-		[]string{"-V", "-no-datetime", "-e", "go run test/command.go -comp", "-c", "test/compressed.conf"},
+		[]string{"-V", "-no-datetime", "-e", command + "-comp", "-c", "fixtures/compressed.conf"},
 		"test",
 		amqp.Publishing{ContentType: "text/plain", Body: []byte("compressed")},
 		[]string{},
 	},
 	{
 		"output",
-		[]string{"-V", "-no-datetime", "-o", "-e", "go run test/command.go -output=-", "-c", "test/default.conf"},
+		[]string{"-V", "-no-datetime", "-o", "-e", command + "-output=-", "-c", "fixtures/default.conf"},
 		"test",
 		amqp.Publishing{ContentType: "text/plain", Body: []byte("output")},
 		[]string{},
 	},
 	{
 		"queueName",
-		[]string{"-V", "-no-datetime", "-q", "altTest", "-e", "go run test/command.go", "-c", "test/default.conf"},
+		[]string{"-V", "-no-datetime", "-q", "altTest", "-e", command, "-c", "fixtures/default.conf"},
 		"altTest",
 		amqp.Publishing{ContentType: "text/plain", Body: []byte("queueName")},
 		[]string{},
 	},
 	{
 		"properties",
-		[]string{"-V", "-no-datetime", "-i", "-e", "go run test/command.go", "-c", "test/default.conf"},
+		[]string{"-V", "-no-datetime", "-i", "-e", command, "-c", "fixtures/default.conf"},
 		"test",
 		amqp.Publishing{
 			ContentType:   "text/plain",
@@ -68,35 +74,35 @@ var tests = []struct {
 	},
 	{
 		"amqpUrl",
-		[]string{"-V", "-no-datetime", "-e", "go run test/command.go", "-c", "test/amqp_url.conf"},
+		[]string{"-V", "-no-datetime", "-e", command, "-c", "fixtures/amqp_url.conf"},
 		"test",
 		amqp.Publishing{ContentType: "text/plain", Body: []byte("amqpUrl")},
 		[]string{},
 	},
 	{
 		"noAmqpUrl",
-		[]string{"-V", "-no-datetime", "-e", "go run test/command.go", "-c", "test/no_amqp_url.conf"},
+		[]string{"-V", "-no-datetime", "-e", command, "-c", "fixtures/no_amqp_url.conf"},
 		"test",
 		amqp.Publishing{ContentType: "text/plain", Body: []byte("noAmqpUrl")},
 		[]string{},
 	},
 	{
 		"envAmqpUrl",
-		[]string{"-V", "-no-datetime", "-e", "go run test/command.go", "-c", "test/no_amqp_url.conf"},
+		[]string{"-V", "-no-datetime", "-e", command, "-c", "fixtures/no_amqp_url.conf"},
 		"test",
 		amqp.Publishing{ContentType: "text/plain", Body: []byte("envAmqpUrl")},
 		[]string{"AMQP_URL=amqp://guest:guest@localhost"},
 	},
 	{
 		"pipe",
-		[]string{"-V", "-no-datetime", "-pipe", "-e", "go run test/command.go -pipe", "-c", "test/default.conf"},
+		[]string{"-V", "-no-datetime", "-pipe", "-e", command + "-pipe", "-c", "fixtures/default.conf"},
 		"test",
 		amqp.Publishing{ContentType: "text/plain", Body: []byte("pipe")},
 		[]string{},
 	},
 }
 
-func TestMain(m *testing.M) {
+func prepare() *amqp.Connection {
 	makeCmd := exec.Command("make", "build")
 	if err := makeCmd.Run(); err != nil {
 		fmt.Fprintf(os.Stderr, "could not build binary for: %v\n", err)
@@ -115,14 +121,17 @@ func TestMain(m *testing.M) {
 		os.Exit(1)
 	}
 
-	os.Exit(m.Run())
+	conn, err := newConnection("amqp://guest:guest@localhost:5672/")
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "failed to open AMQP connection: %v", err)
+		os.Exit(1)
+	}
+
+	return conn
 }
 
 func TestEndToEnd(t *testing.T) {
-	conn, err := newConnection("amqp://guest:guest@localhost:5672/")
-	if err != nil {
-		t.Errorf("failed to open AMQP connection: %v", err)
-	}
+	conn := prepare()
 	defer conn.Close()
 
 	ch, err := conn.Channel()
@@ -155,10 +164,92 @@ func TestEndToEnd(t *testing.T) {
 			}
 
 			output, _ := ioutil.ReadFile("./command.log")
-			goldie.Assert(t, test.name+"Command", output)
-			goldie.Assert(t, test.name+"Output", stdout.Bytes())
-			goldie.Assert(t, test.name+"Error", stderr.Bytes())
+			goldie.Assert(t, t.Name()+"Command", output)
+			goldie.Assert(t, t.Name()+"Output", stdout.Bytes())
+			goldie.Assert(t, t.Name()+"Error", stderr.Bytes())
 		})
+	}
+}
+
+func TestHelperProcess(t *testing.T) {
+	if os.Getenv("GO_WANT_HELPER_PROCESS") != "1" {
+		return
+	}
+	defer os.Exit(0)
+
+	args := os.Args
+	for len(args) > 0 {
+		if args[0] == "--" {
+			args = args[1:]
+			break
+		}
+		args = args[1:]
+	}
+	cli := flag.NewFlagSet(os.Args[0], flag.ExitOnError)
+	outputFile := cli.String("output", "./command.log", "the output file")
+	isCompressed := cli.Bool("comp", false, "whether the argument is compressed or not")
+	isPipe := cli.Bool("pipe", false, "whether the argument passed via stdin (TRUE) or argument (FALSE)")
+	cli.Parse(args)
+
+	var f io.Writer
+	f = os.Stdout
+	if *outputFile != "-" {
+		of, err := os.Create(*outputFile)
+		if err != nil {
+			fmt.Fprintf(os.Stderr, "failed to open output file: %v\n", err)
+			os.Exit(1)
+		}
+		defer of.Close()
+		f = of
+	}
+	f.Write([]byte("Got executed\n"))
+
+	var message []byte
+	if *isPipe {
+		var err error
+
+		pipe := os.NewFile(3, "/proc/self/fd/3")
+		defer pipe.Close()
+		metadata, err := ioutil.ReadAll(pipe)
+		if err != nil {
+			fmt.Fprintf(os.Stderr, "failed to read metadata from pipe: %v", err)
+			os.Exit(1)
+		}
+
+		f.Write(metadata)
+		f.Write([]byte("\n"))
+
+		message, err = ioutil.ReadAll(os.Stdin)
+		if err != nil {
+			fmt.Fprintf(os.Stderr, "failed to read body from pipe: %v", err)
+			os.Exit(1)
+		}
+	} else {
+		message = []byte(os.Args[len(os.Args)-1])
+	}
+
+	f.Write(message)
+	f.Write([]byte("\n"))
+
+	if !*isPipe {
+		var r io.Reader
+		r = base64.NewDecoder(base64.StdEncoding, bytes.NewBuffer(message))
+		if *isCompressed {
+			zr, err := zlib.NewReader(r)
+			if err != nil {
+				fmt.Fprintf(os.Stderr, "failed to create zlib reader: %v\n", err)
+				os.Exit(1)
+			}
+			defer zr.Close()
+			r = zr
+		}
+		original, err := ioutil.ReadAll(r)
+		if err != nil {
+			fmt.Fprintf(os.Stderr, "failed to decompress input: %v\n", err)
+			os.Exit(1)
+		}
+		f.Write(original)
+		f.Write([]byte("\n"))
 	}
 }
 
@@ -194,7 +285,7 @@ func newCommand(env []string, arg ...string) (cmd *exec.Cmd, stdout *bytes.Buffe
 	cmd = exec.Command("./rabbitmq-cli-consumer", arg...)
 	cmd.Stdout = stdout
 	cmd.Stderr = stderr
-	cmd.Env = append(os.Environ(), env...)
+	cmd.Env = append(append(os.Environ(), "GO_WANT_HELPER_PROCESS=1"), env...)
 	return cmd, stdout, stderr
 }
 
