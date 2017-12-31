26b40ab98a2f2089c1faf95e3630a1cd859212b6
Christian Haeusler <christian.haeusler@bexio.com>
2017-12-31 23:13:59 +0100

    Test LoadConfiguration

--- main_test.go
+++ main_test.go
@@ -201,6 +201,79 @@ func TestExitErrHandler(t *testing.T) {
 
 }
 
+var loadConfigurationTest = []struct{
+	name string
+	flags func(set *flag.FlagSet)
+	err error
+	url string
+	queue string
+}{
+	{
+		"default",
+		func(set *flag.FlagSet) {
+			set.String("configuration", "fixtures/default.conf", "")
+		},
+		nil,
+		"",
+		"test",
+	},
+	{
+		"missingFlags",
+		func(set *flag.FlagSet) {},
+		cli.NewExitError("", 1),
+		"",
+		"",
+	},
+	{
+		"missingFile",
+		func(set *flag.FlagSet) {
+			set.String("configuration", "/does/not/exits.conf", "")
+		},
+		fmt.Errorf("failed parsing configuration: open /does/not/exits.conf: no such file or directory"),
+		"",
+		"",
+	},
+	{
+		"url",
+		func(set *flag.FlagSet) {
+			set.String("configuration", "fixtures/default.conf", "")
+			set.String("url", "amqp://amqp.example.com:1234", "")
+		},
+		nil,
+		"amqp://amqp.example.com:1234",
+		"test",
+	},
+	{
+		"queue",
+		func(set *flag.FlagSet) {
+			set.String("configuration", "fixtures/default.conf", "")
+			set.String("queue-name", "someOtherQueue", "")
+		},
+		nil,
+		"",
+		"someOtherQueue",
+	},
+}
+
+func TestLoadConfiguration(t *testing.T) {
+	app := main.NewApp()
+	app.Writer = &bytes.Buffer{}
+	for _, test := range loadConfigurationTest {
+		t.Run(test.name, func(t *testing.T) {
+			set := flag.NewFlagSet(t.Name(), flag.ContinueOnError)
+			test.flags(set)
+			c := cli.NewContext(app, set, nil)
+			cfg, err := main.LoadConfiguration(c)
+			tassert.Equal(t, err, test.err)
+			if test.err == nil {
+				tassert.NotNil(t, cfg)
+				tassert.Equal(t, cfg.RabbitMq.AmqpUrl, test.url)
+				tassert.Equal(t, cfg.RabbitMq.Queue, test.queue)
+			}
+		})
+	}
+}
+
 func createLogger(name string, verbose, noDateTime bool) (*log.Logger, *os.File, *bytes.Buffer, error) {
 	buf := &bytes.Buffer{}
 	f, err := ioutil.TempFile("", name)
