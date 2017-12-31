2c3056f17613e4a0e8525e110d56d0ed551d6cef
Christian Haeusler <christian.haeusler@bexio.com>
2017-12-31 23:45:31 +0100

    Splitt main test file

---
+++ config_test.go
@@ -0,0 +1,84 @@
+package main_test
+
+import (
+	"bytes"
+	"flag"
+	"fmt"
+	"testing"
+
+	"github.com/codegangsta/cli"
+	"github.com/corvus-ch/rabbitmq-cli-consumer"
+)
+
+var loadConfigurationTest = []struct {
+	name  string
+	flags func(set *flag.FlagSet)
+	err   error
+	url   string
+	queue string
+}{
+	{
+		"default",
+		func(set *flag.FlagSet) {
+			set.String("configuration", "fixtures/default.conf", "")
+		},
+		nil,
+		"",
+		"test",
+	},
+	{
+		"missingFlags",
+		func(set *flag.FlagSet) {},
+		cli.NewExitError("", 1),
+		"",
+		"",
+	},
+	{
+		"missingFile",
+		func(set *flag.FlagSet) {
+			set.String("configuration", "/does/not/exits.conf", "")
+		},
+		fmt.Errorf("failed parsing configuration: open /does/not/exits.conf: no such file or directory"),
+		"",
+		"",
+	},
+	{
+		"url",
+		func(set *flag.FlagSet) {
+			set.String("configuration", "fixtures/default.conf", "")
+			set.String("url", "amqp://amqp.example.com:1234", "")
+		},
+		nil,
+		"amqp://amqp.example.com:1234",
+		"test",
+	},
+	{
+		"queue",
+		func(set *flag.FlagSet) {
+			set.String("configuration", "fixtures/default.conf", "")
+			set.String("queue-name", "someOtherQueue", "")
+		},
+		nil,
+		"",
+		"someOtherQueue",
+	},
+}
+
+func TestLoadConfiguration(t *testing.T) {
+	app := main.NewApp()
+	app.Writer = &bytes.Buffer{}
+	for _, test := range loadConfigurationTest {
+		t.Run(test.name, func(t *testing.T) {
+			set := flag.NewFlagSet(t.Name(), flag.ContinueOnError)
+			test.flags(set)
+			c := cli.NewContext(app, set, nil)
+			cfg, err := main.LoadConfiguration(c)
+			tassert.Equal(t, err, test.err)
+			if test.err == nil {
+				tassert.NotNil(t, cfg)
+				tassert.Equal(t, cfg.RabbitMq.AmqpUrl, test.url)
+				tassert.Equal(t, cfg.RabbitMq.Queue, test.queue)
+			}
+		})
+	}
+}
---
+++ error_test.go
@@ -0,0 +1,77 @@
+package main_test
+
+import (
+	"bytes"
+	"fmt"
+	"log"
+	"os"
+	"testing"
+
+	"github.com/bouk/monkey"
+	"github.com/codegangsta/cli"
+	"github.com/corvus-ch/rabbitmq-cli-consumer"
+	"github.com/pkg/errors"
+	"github.com/stretchr/testify/assert"
+)
+
+var exitErrHandlerTests = []struct {
+	name string
+	err  error
+	out  string
+	exit string
+}{
+	{
+		"empty",
+		nil,
+		"",
+		"",
+	},
+	{
+		"exitCode",
+		cli.NewExitError("", 42),
+		"",
+		"os.Exit called with: 42",
+	},
+	{
+		"output",
+		fmt.Errorf("normal error"),
+		"normal error\n",
+		"os.Exit called with: 1",
+	},
+	{
+		"exitCodeOutput",
+		cli.NewExitError("exit code error", 42),
+		"exit code error\n",
+		"os.Exit called with: 42",
+	},
+	{
+		"outputFormatted",
+		errors.WithMessage(fmt.Errorf("error"), "nested"),
+		"error\nnested\n",
+		"os.Exit called with: 1",
+	},
+}
+
+func TestExitErrHandler(t *testing.T) {
+	log.SetFlags(0)
+	patch := monkey.Patch(os.Exit, func(code int) {
+		panic(fmt.Sprintf("os.Exit called with: %v", code))
+	})
+	defer patch.Unpatch()
+	for _, test := range exitErrHandlerTests {
+		t.Run(test.name, func(t *testing.T) {
+			buf := &bytes.Buffer{}
+			log.SetOutput(buf)
+			h := func() {
+				main.ExitErrHandler(nil, test.err)
+			}
+			if test.exit == "" {
+				h()
+			} else {
+				assert.PanicsWithValue(t, test.exit, h, "os.Exit was not called")
+			}
+			assert.Equal(t, test.out, buf.String())
+		})
+	}
+
+}
